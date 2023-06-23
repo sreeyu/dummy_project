5207bddf7d2e5111405157cf36c92240f3f2ddac
sreey <sreelakshmi12898@gmail.com>
2023-06-23 20:21:53 +0530

    Display paragraph upon button click using useState

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from './App.module.css';
 import Button from './components/UI/Button';
 
 function App() {
 
+  const[showPara, setShowPara] = useState(false);
+
+  const getPara = () => {
+    setShowPara(prevParaState => !prevParaState);
+  };
+
   return (
     <div className={styles.app}>
       <h1>Hello</h1>
-      <Button >Click for para</Button>
+      {showPara && <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua</p>}
+      <Button onClick={getPara} >Click for para</Button>
     </div>
   );
 }
