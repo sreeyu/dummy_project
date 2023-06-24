1e6b1a3ddbb324d1498955a8b8e4020370af6a3e
sreey <sreelakshmi12898@gmail.com>
2023-06-24 19:46:43 +0530

    Revaluate components only on props change using React.memo

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,7 @@ function App() {
   return (
     <div className={styles.app}>
       <h1>Hello</h1>
-      <Demo show={showPara} />
+      <Demo show={false} />
       <Button onClick={getPara} >Click for para</Button>
     </div>
   );
--- src/components/Demo/Demo.jsx
+++ src/components/Demo/Demo.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 
 function Demo(props){
+
+    console.log("Demo running");
     
     return(
         <p>{props.show ? "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua" : ''}</p>
     )
 };
 
-export default Demo;
+export default React.memo(Demo);
